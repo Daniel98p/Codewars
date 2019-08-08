cd2de2141bd7f1e01180b804f97fe41e32a45236
Daniel <danielmichalik98@wp.pl>
2019-08-08 14:32:50 +0200

    Updated Scramblies

--- Scramblies.py
+++ Scramblies.py
@@ -1,18 +1,7 @@
 def scramble(s1, s2):
-    repeated_value = 0
-    repeated_value1 = 0
-    for iterator in s2:
-        for iterator1 in s2:
-            if iterator1 == iterator:
-                repeated_value += 1
-
-    for iterator2 in s1:
-        for iterator3 in s1:
-            if iterator2 == iterator3:
-                repeated_value1 += 1
-    if repeated_value > repeated_value1:
-        return False
-
+    for value in s2:
+        if s1.count(value) < s2.count(value):
+            return False
     set1 = set(s2)
     set2 = set(s1)
     if set1.issubset(set2):
